4f7d578582d18a58bf86d6364fc39f98cacdd0d2
Marek Marcola <Marek.Marcola@gmail.com>
2026-09-14 21:47:11 +0200

    zlocal-bman.sh: add cdbm shell function
    Change-Id: I6791bd5dde40df75b60464d7b6c9981cd5ef2bb5

--- zlocal-bman.sh
+++ zlocal-bman.sh
@@ -2,3 +2,10 @@ bm() {
   local desc="@@netboot management (via bman.sh)@@"
   bman.sh $@
 }
+
+cdbm() {
+  local desc="@@change working directory to bman $EDIR@@"
+  local D="/usr/local/etc/bman.d"
+  cd $D
+  pwd
+}
